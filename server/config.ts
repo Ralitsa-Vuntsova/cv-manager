f2614082a6c4f2afbd549f2af0b439e7b8363539
ralitsa-vuntsova <ralitsavun@gmail.com>
2023-12-04 10:48:15 +0200

    Add users

---
+++ server/config.ts
@@ -0,0 +1,14 @@
+import { config as envConfig } from 'dotenv';
+envConfig();
+
+const config = {
+  db: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
+};
+
+export { config };
--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,5 +1,7 @@
 // Update with your config settings.
 
+import { config } from "./config";
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -7,13 +9,7 @@ const knexConfig = {
 
   development: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection: config.db,
     pool: {
       min: 2,
       max: 10
